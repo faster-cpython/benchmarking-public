73e37f2fa258d3d3ab90990465a1e9f7088b7216
Michael Droettboom <mdboom@gmail.com>
2022-12-16 12:37:15 -0500

    Fix on Mac

--- scripts/run_benchmarks.py
+++ scripts/run_benchmarks.py
@@ -22,7 +22,7 @@ def run_benchmarks(python, benchmarks):
 
     returncode = subprocess.call(
         [
-            "python",
+            "python3",
             "-m",
             "pyperformance",
             "run",

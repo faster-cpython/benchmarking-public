57b1ccee8eea5181ad31eea50334568a91ad606a
Michael Droettboom <mdboom@gmail.com>
2022-12-21 16:57:22 -0500

    Find the git merge base in a more fool-proof way

--- scripts/get_merge_base.py
+++ scripts/get_merge_base.py
@@ -8,13 +8,12 @@ sys.path.insert(0, str(Path(__file__).parent))
 from lib import _git
 
 
-if __name__ == "__main__":
-    hard_coded = sys.argv[-1] != "false"
-    if not hard_coded:
+def main(need_to_run: bool, cpython: Path = Path("cpython")) -> None:
+    if not need_to_run:
         print("ref=xxxxxxx")
         print("need_to_run=false")
     else:
-        merge_base = _git.get_git_merge_base(Path("cpython"))
+        merge_base = _git.get_git_merge_base(cpython)
 
         need_to_run = True
         for entry in Path("results").iterdir():
@@ -25,3 +24,9 @@ if __name__ == "__main__":
 
         print(f"ref={merge_base}")
         print(f"need_to_run={str(need_to_run).lower()}")
+
+
+if __name__ == "__main__":
+    need_to_run = sys.argv[-1] != "false"
+
+    main(need_to_run)
---
+++ tests/test_git_merge_base.py
@@ -0,0 +1,39 @@
+import subprocess
+import textwrap
+
+
+from scripts import get_merge_base
+
+
+def test_get_merge_base(tmp_path, capsys):
+    subprocess.check_call(
+        [
+            "git",
+            "clone",
+            "https://github.com/mdboom/cpython",
+            "--branch",
+            "fix-pystats",
+            "--depth",
+            "50",
+        ],
+        cwd=tmp_path,
+    )
+
+    get_merge_base.main(True, tmp_path / "cpython")
+
+    captured = capsys.readouterr()
+
+    assert captured.out.strip() == textwrap.dedent(
+        """
+    ref=158b8a07212cea6066afe8bb91f1cd542d922dba
+    need_to_run=true
+    """
+    ).strip()
+
+
+def test_hard_coded(capsys):
+    get_merge_base.main(False)
+
+    captured = capsys.readouterr()
+
+    assert "need_to_run=false" in captured.out

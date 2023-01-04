afc127ed5bc380cc208ad2b4b9d387d0c3904fd9
Michael Droettboom <mdboom@gmail.com>
2023-01-04 10:44:17 -0500

    Increase test coverage

--- tests/test_generate_results.py
+++ tests/test_generate_results.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import shutil
 
 
+import pytest
+
+
 from scripts import generate_results
 
 
@@ -16,26 +19,10 @@ def _copy_repo(tmp_path):
     return repo_path
 
 
-def test_main(tmp_path):
-    repo_path = _copy_repo(tmp_path)
+def _run_for_bases(bases, repo_path, force=False, has_base=[]):
     results_path = repo_path / "results"
 
-    # Hack up so one of the results has an explicit commit_merge_base
-    result_with_base = (
-        results_path
-        / "bm-20221119-3.12.0a3+-b0e1f9c"
-        / "bm-20221119-linux-x86_64-python-main-3.12.0a3+-b0e1f9c.json"
-    )
-    with open(result_with_base) as fd:
-        contents = json.load(fd)
-    contents["metadata"][
-        "commit_merge_base"
-    ] = "9d38120e335357a3b294277fd5eff0a10e46e043"
-    with open(result_with_base, "w") as fd:
-        json.dump(contents, fd)
-    # End hack
-
-    generate_results.main(repo_path, bases=["3.10.4", "3.11.0b3"])
+    generate_results.main(repo_path, force=force, bases=bases)
 
     for dirpath in results_path.iterdir():
         if not dirpath.is_dir():
@@ -46,10 +33,10 @@ def test_main(tmp_path):
             files_by_type[filepath.suffix] += 1
         assert files_by_type[".json"] == 1
 
-        if "b0e1f9c" in dirpath.name:
+        if any(base in dirpath.name for base in has_base):
             assert files_by_type[".md"] == 4
             assert files_by_type[".png"] == 3
-        elif "3.10.4" in dirpath.name or "3.11.0b3" in dirpath.name:
+        elif any(base in dirpath.name for base in bases):
             assert files_by_type[".md"] == 2
             assert files_by_type[".png"] == 1
         else:
@@ -65,3 +52,43 @@ def test_main(tmp_path):
 
     contents = (repo_path / "results" / "README.md").read_text()
     assert contents.count("\n|") == 18
+
+
+def test_main(tmp_path):
+    repo_path = _copy_repo(tmp_path)
+
+    # Hack up so one of the results has an explicit commit_merge_base
+    result_with_base = (
+        repo_path
+        / "results"
+        / "bm-20221119-3.12.0a3+-b0e1f9c"
+        / "bm-20221119-linux-x86_64-python-main-3.12.0a3+-b0e1f9c.json"
+    )
+    with open(result_with_base) as fd:
+        contents = json.load(fd)
+    contents["metadata"][
+        "commit_merge_base"
+    ] = "9d38120e335357a3b294277fd5eff0a10e46e043"
+    with open(result_with_base, "w") as fd:
+        json.dump(contents, fd)
+    # End hack
+
+    # Run twice to make sure there are no side effects of that
+    _run_for_bases(["3.10.4", "3.11.0b3"], repo_path, has_base=["b0e1f9c"])
+    _run_for_bases(["3.10.4", "3.11.0b3"], repo_path, has_base=["b0e1f9c"])
+
+
+def test_change_bases(tmp_path):
+    repo_path = _copy_repo(tmp_path)
+
+    _run_for_bases(["3.10.4", "3.11.0b3"], repo_path)
+    _run_for_bases(["3.10.4", "3.11.0b2"], repo_path, force=True)
+
+    (
+        repo_path / "results"
+        / "bm-20221119-3.12.0a3+-b0e1f9c"
+        / "bm-20221119-linux-x86_64-python-main-3.12.0a3+-b0e1f9c.json"
+    ).unlink()
+
+    with pytest.raises(ValueError):
+        generate_results.main(repo_path, bases=[])
--- tests/test_run_benchmarks.py
+++ tests/test_run_benchmarks.py
@@ -1,7 +1,9 @@
 import json
 from pathlib import Path
+import platform
 import shutil
 import subprocess
+import sys
 
 
 from scripts import run_benchmarks
@@ -39,8 +41,6 @@ def test_update_metadata(tmp_path):
 
     metadata = content["metadata"]
 
-    print(metadata)
-
     assert metadata["commit_id"] == "9d38120"
     assert metadata["commit_fork"] == "myfork"
     assert metadata["commit_branch"] == "myref"
@@ -48,3 +48,102 @@ def test_update_metadata(tmp_path):
     assert "commit_merge_base" not in metadata
     assert metadata["benchmark_hash"] == "e3b0c4"
     assert not metadata.get("publish")
+
+
+def test_run_benchmarks(tmp_path):
+    # This is mainly recreating the first few steps in _benchmark.yml, except
+    # for compiling Python from scratch
+    subprocess.check_call(
+        [
+            "git",
+            "clone",
+            "https://github.com/python/cpython",
+            "--depth",
+            "1",
+            "--branch",
+            "v3.10.4",
+        ],
+        cwd=tmp_path,
+    )
+    subprocess.check_call(
+        [
+            "git",
+            "clone",
+            "https://github.com/pyston/python-macrobenchmarks",
+            "--depth",
+            "1",
+            "pyston-benchmarks",
+        ],
+        cwd=tmp_path,
+    )
+    subprocess.check_call(
+        [
+            "git",
+            "clone",
+            "https://github.com/mdboom/pyperformance",
+            "--depth",
+            "1",
+            "--branch",
+            "fix-manifest-parsing",
+        ],
+        cwd=tmp_path,
+    )
+
+    venv_dir = tmp_path / "venv"
+    venv_python = venv_dir / "bin" / "python"
+
+    subprocess.check_call([sys.executable, "-m", "venv", venv_dir], cwd=tmp_path)
+    subprocess.check_call(
+        [venv_python, "-m", "pip", "install", "setuptools", "wheel"], cwd=tmp_path
+    )
+    subprocess.check_call(
+        [venv_python, "-m", "pip", "install", tmp_path / "pyperformance"], cwd=tmp_path
+    )
+
+    shutil.copy(
+        Path(__file__).parents[1] / "benchmarks.manifest",
+        tmp_path / "benchmarks.manifest",
+    )
+
+    # Now actually run the run_benchmarks.py script
+    subprocess.check_call(
+        [
+            venv_python,
+            run_benchmarks.__file__,
+            "benchmark",
+            sys.executable,
+            "python",
+            "main",
+            "deepcopy",
+            "false",
+        ],
+        cwd=tmp_path,
+    )
+
+    with open(
+        tmp_path
+        / "results"
+        / f"bm-20220323-{platform.python_version()}-9d38120"
+        / f"bm-20220323-{platform.system().lower()}-{platform.machine()}-"
+        f"python-main-{platform.python_version()}-9d38120.json"
+    ) as fd:
+        content = json.load(fd)
+
+    metadata = content["metadata"]
+    benchmarks = content["benchmarks"]
+
+    assert metadata["commit_id"] == "9d38120"
+    assert metadata["commit_fork"] == "python"
+    assert metadata["commit_branch"] == "main"
+    assert metadata["commit_date"] == "2022-03-23T20:12:04+00:00"
+    assert "commit_merge_base" not in metadata
+    assert metadata["benchmark_hash"] == "9d2e5f"
+    assert not metadata.get("publish")
+
+    assert len(benchmarks) == 3
+    assert all(len(benchmark["runs"]) > 1 for benchmark in benchmarks)
+    assert set(bm["metadata"]["name"] for bm in benchmarks) == {
+        "deepcopy",
+        "deepcopy_memo",
+        "deepcopy_reduce",
+    }

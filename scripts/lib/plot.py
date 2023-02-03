224f1faf55ea600f2856d9adfa6f6a18747f9385
Michael Droettboom <mdboom@gmail.com>
2023-02-02 19:22:11 -0500

    Sort results in plots from fastest to slowest

--- scripts/lib/plot.py
+++ scripts/lib/plot.py
@@ -33,21 +33,13 @@ def remove_outliers(values, m=2):
     return values[abs(values - np.mean(values)) < m * np.std(values)]
 
 
-def plot_diff_pair(ax, ref, head, names, outlier_rejection=True):
+def plot_diff_pair(ax, data):
     master_data = []
     all_data = []
 
-    for name in names:
-        ref_values = ref[name]
-        head_values = head[name]
-        if outlier_rejection:
-            ref_values = remove_outliers(ref_values)
-            head_values = remove_outliers(head_values)
-        values = np.outer(ref_values, 1.0 / head_values).flatten()
-        values.sort()
+    for name, values, mean in data:
         idx = np.round(np.linspace(0, len(values) - 1, 100)).astype(int)
         all_data.append(values[idx])
-
         master_data.extend(values)
 
     all_data.append(master_data)
@@ -70,20 +62,34 @@ def formatter(val, pos):
     return f"{val:.02f}x"
 
 
+def calculate_diffs(ref_values, head_values, outlier_rejection=True):
+    if outlier_rejection:
+        ref_values = remove_outliers(ref_values)
+        head_values = remove_outliers(head_values)
+    values = np.outer(ref_values, 1.0 / head_values).flatten()
+    values.sort()
+    return values, values.mean()
+
+
 def plot_diff(
     ref: result.Result, head: result.Result, output_filename: Path, title: str
 ) -> None:
     ref_data = get_data(ref)
     head_data = get_data(head)
 
-    names = sorted(
-        set(ref_data.keys()) & set(head_data.keys()),
-        reverse=True,
-    )
+    combined_data = [
+        (name, *calculate_diffs(ref, head_data[name]))
+        for name, ref in ref_data.items()
+        if name in head_data
+    ]
+    combined_data.sort(key=lambda x: x[2])
 
-    fig, axs = plt.subplots(figsize=(8, 2 + len(names) * 0.3), layout="constrained")
+    fig, axs = plt.subplots(
+        figsize=(8, 2 + len(combined_data) * 0.3), layout="constrained"
+    )
     plt.axvline(1.0)
-    plot_diff_pair(axs, ref_data, head_data, names)
+    plot_diff_pair(axs, combined_data)
+    names = [x[0] for x in combined_data]
     names.append("ALL")
     axs.set_yticks(np.arange(len(names)) + 1, names)
     axs.set_ylim(0, len(names) + 1)

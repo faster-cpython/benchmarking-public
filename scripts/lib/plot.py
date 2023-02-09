87de38df53552891f0bd297bfda3eb628cbd678d
Michael Droettboom <mdboom@gmail.com>
2023-02-09 12:12:28 -0500

    Ignore insignificant benchmarks in plots

--- scripts/lib/plot.py
+++ scripts/lib/plot.py
@@ -5,6 +5,7 @@ from typing import Any, Dict
 from matplotlib import pyplot as plt
 import matplotlib
 import numpy as np
+import pyperf
 
 
 matplotlib.use("agg")
@@ -37,10 +38,14 @@ def plot_diff_pair(ax, data):
     master_data = []
     all_data = []
 
-    for name, values, mean in data:
-        idx = np.round(np.linspace(0, len(values) - 1, 100)).astype(int)
-        all_data.append(values[idx])
-        master_data.extend(values)
+    for i, (name, values, mean) in enumerate(data):
+        if values is not None:
+            idx = np.round(np.linspace(0, len(values) - 1, 100)).astype(int)
+            all_data.append(values[idx])
+            master_data.extend(values)
+        else:
+            all_data.append([1.0])
+            ax.text(1.01, i + 1, "insignificant")
 
     all_data.append(master_data)
 
@@ -63,12 +68,17 @@ def formatter(val, pos):
 
 
 def calculate_diffs(ref_values, head_values, outlier_rejection=True):
-    if outlier_rejection:
-        ref_values = remove_outliers(ref_values)
-        head_values = remove_outliers(head_values)
-    values = np.outer(ref_values, 1.0 / head_values).flatten()
-    values.sort()
-    return values, values.mean()
+    sig, t_score = pyperf._utils.is_significant(ref_values, head_values)
+
+    if not sig:
+        return None, 0
+    else:
+        if outlier_rejection:
+            ref_values = remove_outliers(ref_values)
+            head_values = remove_outliers(head_values)
+        values = np.outer(ref_values, 1.0 / head_values).flatten()
+        values.sort()
+        return values, values.mean()
 
 
 def plot_diff(

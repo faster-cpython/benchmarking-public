c798500a68a5177941b8f767f0c81c5871868c29
Michael Droettboom <mdboom@gmail.com>
2023-04-19 09:08:05 -0400

    Lazy import pkg_version

--- scripts/lib/result.py
+++ scripts/lib/result.py
@@ -9,9 +9,6 @@ import subprocess
 from typing import Any, Dict, Iterable, List, Optional, Set, Tuple
 
 
-from packaging import version as pkg_version
-
-
 from lib import git
 from lib import runners
 
@@ -366,6 +363,8 @@ class Result:
 
     @functools.cached_property
     def parsed_version(self) -> pkg_version.Version:
+        from packaging import version as pkg_version
+
         return pkg_version.parse(self.version.replace("+", "0"))
 
     def match_to_bases(self, bases: List[str], results: Iterable["Result"]) -> None:

b29ce5cebcd81914a7f5989f1c120430b4138b11
Michael Droettboom <mdboom@gmail.com>
2023-05-09 08:36:01 -0400

    Fix paths that profiling results are generated in

--- scripts/generate_results.py
+++ scripts/generate_results.py
@@ -369,7 +369,9 @@ def main(repo_dir: Path, force: bool = False, bases: Optional[List[str]] = None)
     print("Generating longitudinal plot")
     plot.longitudinal_plot(results, repo_dir / "longitudinal.png")
     print("Generating profiling plot")
-    profiling_plot.generate_results(repo_dir, repo_dir / "profiling" / "results")
+    profiling_plot.generate_results(
+        repo_dir / "profiling", repo_dir / "profiling" / "results"
+    )
 
 
 if __name__ == "__main__":
